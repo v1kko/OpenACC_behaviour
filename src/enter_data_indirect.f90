8008c338a21f00b56ed0b33963107110171a17f9
Victor Azizi <victor@lipsum.eu>
2026-02-10 09:44:32 +0100

    remove unnecessary lines

--- src/enter_data_indirect.f90
+++ src/enter_data_indirect.f90
@@ -1,5 +1,4 @@
 program main
-  use iso_c_binding, only: c_size_t
   use openacc
   implicit none
   type container_type
@@ -8,8 +7,6 @@ program main
 
   real, dimension(:) :: host_mem(2) = (/4,2/), res(2)
   integer :: i
-  type(c_devptr) :: dev_ptr
-  integer(c_size_t) :: dev_size
 
   type(container_type) :: container
   !$acc enter data create(container)
