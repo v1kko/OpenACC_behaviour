e3143540bf7cc908a878d0e90270f46bcb06665e
Victor Azizi <victor@lipsum.eu>
2026-04-03 11:35:09 +0200

    update files

---
+++ src/enter_data_members.f90
@@ -0,0 +1,25 @@
+program main
+  use openacc
+  implicit none
+  type container_type
+      real, dimension(:), allocatable :: values
+  end type
+
+  real, dimension(:) :: host_mem(2) = (/4,2/), res(2)
+  integer :: i
+
+  type(container_type) :: container
+  !$acc enter data create(container)
+  !$acc enter data create(container%values)
+
+  allocate(container%values(2))
+  container%values = host_mem
+
+  write(*,*) "Checking if allocatable values are copied to device after enter data create ..."
+  !$acc parallel loop 
+  do i=1,2 
+    res(i) = container%values(i)
+  enddo
+  write(*,*) "Success", res
+end program
+  
