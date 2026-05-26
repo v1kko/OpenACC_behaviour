14140d981354c598f895684f2a8b1143e62bff02
Victor Azizi <victor@lipsum.eu>
2026-05-26 13:33:32 +0200

    use slicing inside openmpi

--- src/gpu_aware_mpi.f90
+++ src/gpu_aware_mpi.f90
@@ -4,9 +4,8 @@ program gpu_aware_mpi
   implicit none
 
   integer, parameter :: n = 128
-  integer :: ierr, rank, nprocs, partner, i, mismatches, expected, gpu_num
-  integer :: sentinel
-  integer, allocatable, dimension(:) :: send_buf, recv_buf
+  integer :: ierr, rank, nprocs, partner, i, j, mismatches, expected, gpu_num
+  integer, allocatable, dimension(:,:) :: send_buf, recv_buf
   !$acc declare device_resident(send_buf, recv_buf)
   type(MPI_Status) :: status
 
@@ -26,26 +25,28 @@ program gpu_aware_mpi
 
   partner = 1 - rank
 
-  allocate(send_buf(n))
-  allocate(recv_buf(n))
+  allocate(send_buf(n,n))
+  allocate(recv_buf(n,n))
 
-  !$acc parallel loop present(send_buf, recv_buf)
+  !$acc parallel loop collapse(2) present(send_buf, recv_buf)
   do i = 1, n
-    send_buf(i) = i + rank*1000
-    recv_buf(i) = 0
+    do j = 1, n
+      send_buf(i,j) = i+j*128+rank*17000 
+      recv_buf(i,j) = 0
+    end do
   end do
 
   !$acc host_data use_device(send_buf, recv_buf)
-  call MPI_Sendrecv(send_buf, n, MPI_INTEGER, partner, 0, &
-                    recv_buf, n, MPI_INTEGER, partner, 0, &
+  call MPI_Sendrecv(send_buf(:,6), n, MPI_INTEGER, partner, 0, &
+                    recv_buf(:,5), n, MPI_INTEGER, partner, 0, &
                     MPI_COMM_WORLD, status, ierr)
   !$acc end host_data
 
   mismatches = 0
   !$acc parallel loop present(recv_buf) reduction(+:mismatches) private(expected)
   do i = 1, n
-    expected = i + partner*1000
-    if (recv_buf(i) /= expected) mismatches = mismatches + 1
+    expected = 6*128 + i + partner*1000
+    if (recv_buf(i,5) /= expected) mismatches = mismatches + 1
   end do
 
   if (mismatches /= 0) then
