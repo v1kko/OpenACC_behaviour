0a8cdb024983e346f6c607aa34848d6698e4af4a
Victor Azizi <victor@lipsum.eu>
2026-05-29 14:25:19 +0200

    update lumi script

--- scripts/lumi_cray.sh
+++ scripts/lumi_cray.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-CASE=lumi-cray
+CASE=lumi_cray
 
 ssh lumi "
 module load LUMI/25.03
-module load PrgEnv-cray/8.6.0
 module load partition/G
 
 git clone https://github.com/v1kko/OpenACC_behaviour.git
@@ -16,11 +15,13 @@ git pull
 export COMPILER=\"ftn -g -hacc\"
 export CASE=${CASE}
 export CRAY_ACC_DEBUG=3
+export MPICH_GPU_SUPPORT_ENABLED=1
 
 make clean
 make
 
-export SUBMIT_COMMAND=\"srun -t 1:00 -p standard-g --gres=gpu:1 --ntasks 1 --account project_465002836\"
+export SUBMIT_COMMAND=\"srun -t 1:00 -p dev-g --gres=gpu:1 --ntasks 1 --account project_465002836\"
+export MPI_SUBMIT_COMMAND=\"srun -t 1:00 -p dev-g --gres=gpu:2 --ntasks 2 --account project_465002836\"
 
 make run
 
