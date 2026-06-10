81d1f4a9fea3c61614c832340060af7a45d6f9f4
Victor Azizi <victor@lipsum.eu>
2026-06-10 06:58:03 +0000

    Revert "add partly-present, exit data finalize, and update not-present tests"
    This reverts commit 0cc31fb2c170be21f6bc3fc8d03bcf6fbc69d873.

--- src/update_not_present.f90
+++
@@ -1,9 +0,0 @@
-program main
-  use openacc
-  implicit none
-  real :: host_mem(10)
-
-  host_mem = 4
-  !$acc update device(host_mem)
-  write(*,*) "No runtime error for update of not-present data"
-end program
